3e64d9dcd8d965aa314700bb93960403e9875fa5
iconicbta <iconicbta@outlook.com>
2025-11-12 14:33:39 -0500

    Update pagosLigasController.js

--- backend/controllers/pagosLigasController.js
+++ backend/controllers/pagosLigasController.js
@@ -1,19 +1,28 @@
-exports.obtenerMeses = async (req, res) => {
+// backend/controllers/pagosLigasController.js
+const obtenerMeses = async (req, res) => {
   res.json([{ _id: 1, nombre: "Noviembre 2025" }]);
 };
 
-exports.crearMes = async (req, res) => {
+const crearMes = async (req, res) => {
   res.json({ mensaje: "Mes creado correctamente" });
 };
 
-exports.obtenerPagosPorMes = async (req, res) => {
+const obtenerPagosPorMes = async (req, res) => {
   res.json([]);
 };
 
-exports.registrarPago = async (req, res) => {
+const registrarPago = async (req, res) => {
   res.json({ mensaje: "Pago registrado correctamente" });
 };
 
-exports.actualizarValorDiario = async (req, res) => {
+const actualizarValorDiario = async (req, res) => {
   res.json({ mensaje: "Valor diario actualizado correctamente" });
+};
+
+module.exports = {
+  obtenerMeses,
+  crearMes,
+  obtenerPagosPorMes,
+  registrarPago,
+  actualizarValorDiario,
 };
