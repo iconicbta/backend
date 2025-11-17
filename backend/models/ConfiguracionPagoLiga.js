66ae098acb7b37fdab04de38ebdb9ee4e64768d7
iconicbta <iconicbta@outlook.com>
2025-11-17 10:57:46 -0500

    Update ConfiguracionPagoLiga.js

--- backend/models/ConfiguracionPagoLiga.js
+++ backend/models/ConfiguracionPagoLiga.js
@@ -2,10 +2,7 @@
 const mongoose = require("mongoose");
 
 const configSchema = new mongoose.Schema({
-  valorDiario: {
-    type: Number,
-    default: 8000,
-  },
+  valorDiario: { type: Number, default: 8000 },
 });
 
 module.exports = mongoose.model("ConfiguracionPagoLiga", configSchema);
