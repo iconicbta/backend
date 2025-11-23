633c732ee74e6c0f6217e8df8efeac3d53a5e830
iconicbta <iconicbta@outlook.com>
2025-11-23 08:53:59 -0500

    Update PagoLigaMes.js

--- backend/models/PagoLigaMes.js
+++ backend/models/PagoLigaMes.js
@@ -2,15 +2,20 @@
 const mongoose = require("mongoose");
 
 const pagoLigaMesSchema = new mongoose.Schema({
-  nombre: { type: String, required: true, trim: true, uppercase: true },
-  equipo: { type: String, default: "Ligas" },
-  mes: { type: String, required: true },
-  diasAsistidos: { type: Number, required: true },
-  total: { type: Number, required: true },
-  valorDiarioUsado: { type: Number },
-  diasPagados: { type: [Number], default: [] }, // ← CLAVE
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
+  nombre: { type: String, required: true, trim: true, uppercase: true },
+  equipo: { type: String, default: "Ligas" },
+  mes: { type: String, required: true },
+  diasAsistidos: { type: Number, required: true },
+  total: { type: Number, required: true },
+  valorDiarioUsado: { type: Number },
+  diasPagados: { type: [Number], default: [] }, 
+  tipoPago: { // 🆕 CAMBIO CLAVE
+      type: String, 
+      enum: ['Efectivo', 'Nequi', 'SYSTEM'], 
+      default: 'Efectivo' 
+    },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("PagoLigaMes", pagoLigaMesSchema);
