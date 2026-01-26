3be5eb0e016643567f767e7d424014d3d502fe52
iconicbta <iconicbta@outlook.com>
2026-01-26 18:02:33 -0500

    Update pagaMesModels.js

--- backend/models/pagaMesModels.js
+++ backend/models/pagaMesModels.js
@@ -2,17 +2,17 @@ const mongoose = require("mongoose");
 
 const pagaMesSchema = new mongoose.Schema({
   nombre: { type: String, required: true, trim: true, uppercase: true },
-  equipo: { type: String, default: "General" }, 
-  mes: { type: String, required: true }, // Ej: "Enero 2026"
-  monto: { type: Number, required: true }, // Valor de la mensualidad
+  equipo: { type: String, default: "General" },
+  anio: { type: String, required: true }, // Ej: "2026"
+  plan: { type: String, default: "Black" }, // Black, White, Gold
+  total: { type: Number, required: true },
+  mesesPagados: { type: [String], default: [] }, // Guardará ["Enero", "Marzo"]
   tipoPago: { 
-    type: String, 
-    enum: ['Efectivo', 'Nequi', 'SYSTEM'], 
-    default: 'Efectivo' 
-  },
-  comentario: { type: String, trim: true, default: "" },
+      type: String, 
+      enum: ['Efectivo', 'Nequi', 'SYSTEM'], 
+      default: 'Efectivo' 
+    },
   createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("PagaMes", pagaMesSchema);
