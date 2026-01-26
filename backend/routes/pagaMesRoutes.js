ad589db4e5283533ad2d3f8b458a6134d0d80554
iconicbta <iconicbta@outlook.com>
2026-01-26 18:03:42 -0500

    Update pagaMesRoutes.js

--- backend/routes/pagaMesRoutes.js
+++ backend/routes/pagaMesRoutes.js
@@ -1,26 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const {
-  obtenerMeses,
-  crearMes,
-  obtenerPagosPorMes,
-  registrarPagoMes
+const { 
+    obtenerAnios, 
+    crearAnio, 
+    obtenerPagosPorAnio, 
+    registrarPagoMes 
 } = require("../controllers/pagaMesController");
-const PagaMes = require("../models/pagaMesModels");
 
-router.get("/meses", obtenerMeses);
-router.post("/crear-mes", crearMes);
-router.get("/pagos/:mes", obtenerPagosPorMes);
+router.get("/anios", obtenerAnios);
+router.post("/crear-anio", crearAnio);
+router.get("/pagos/:anio", obtenerPagosPorAnio);
 router.post("/pagos", registrarPagoMes);
-
-// Eliminar un pago específico
-router.delete("/pagos/:id", async (req, res) => {
-  try {
-    await PagaMes.findByIdAndDelete(req.params.id);
-    res.json({ message: "Pago eliminado correctamente" });
-  } catch (error) {
-    res.status(500).json({ message: "Error al eliminar" });
-  }
-});
 
 module.exports = router;
